68423fae78a7f1b81595aac32ecc6b3c3f8ca6ac
Jimmy Fortin <jimmy.fortin@gmail.com>
2026-05-05 20:32:14 -0400

    Add core merge migration to resolve leaf conflict

---
+++ core/migrations/0024_merge_core_0023_leaves.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0023_alter_systemevent_event_type"),
+        ("core", "0023_merge_core_leaf_migrations"),
+    ]
+
+    operations = []
